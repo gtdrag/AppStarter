be26d37189a3b95f1da935cf22af4479f7d65134
George Drag <gdrag@redfoundry.com>
2018-01-15 20:41:19 -0600

    facebook profile work

--- MainProject/SignupViewController.swift
+++ MainProject/SignupViewController.swift
@@ -13,7 +13,7 @@ protocol signupViewControllerDelegate {
     func signupViewControllerDidPressButton(signupViewController: SignupViewController)
 }
 
-class SignupViewController: UIViewController {
+class SignupViewController: UIViewController, FBSDKLoginButtonDelegate {
     var delegate: signupViewControllerDelegate?
     
     @IBOutlet weak var faceBookSignupButton: UIButton!
@@ -28,5 +28,44 @@ class SignupViewController: UIViewController {
         let loginButton = FBSDKLoginButton()
         faceBookSignupButton.addSubview(loginButton)
         loginButton.bindFrameToSuperviewBounds()
+        loginButton.delegate = self
+        
+        if let token = FBSDKAccessToken.current() {
+            fetchProfile()
+        }
+        
+    }
+    
+    func fetchProfile() {
+        
+        FBSDKGraphRequest(graphPath: "me", parameters: ["fields" : "email, first_name, last_name, id, gender, picture.type(large)"])
+            .start(completionHandler:  {
+                (connection, result, error) in
+                if error != nil {
+                    print(error)
+                    return
+                }
+                if let result = result as? NSDictionary {
+                    let email = result["email"] as? String
+                    let user_name = result["first_name"] as? String
+                    let user_gender = result["gender"] as? String
+                    let user_id_fb = result["id"]  as? String
+                    
+                     print(user_name!)
+                }
+    
+            })
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("logged out of FB")
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil {
+            print(error)
+            return
+        }
+        print("success logging into FB")
     }
 }
